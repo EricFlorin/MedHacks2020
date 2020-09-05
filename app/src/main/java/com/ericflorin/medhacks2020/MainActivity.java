0f1628ae178fb7fc37f42806f3daca70f4b79353
EricFlorin <xericfl@gmail.com>
2020-09-05 12:13:36 -0700

    Created the code to display sent and recieved messages

--- app/src/main/java/com/ericflorin/medhacks2020/MainActivity.java
+++ app/src/main/java/com/ericflorin/medhacks2020/MainActivity.java
@@ -4,14 +4,28 @@
 package com.ericflorin.medhacks2020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // For chat message UI
+    private RecyclerView mMessageRecycler;
+    private MessageListAdapter mMessageAdapter;
+    private List<BaseMessage> messageList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initalize our message list
+        messageList = new List<BaseMessage>();
+
+        mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
+        mMessageAdapter = new MessageListAdapter(this, messageList);
+        mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
     }
 }
